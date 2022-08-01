24d1ef0dedba6fce44a3ea54624b1ddcd93f4fcc
Orlando <pereztwinning@gmail.com>
2022-07-31 22:00:33 -0400

    Finished

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -65,7 +62,7 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -77,9 +74,10 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
+
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -97,9 +95,20 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
-        // TODO - implement this method
-        return null;
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> jobItem : job.entrySet()) {
+                if (jobItem.getValue().toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(job);
+                    break;
+                }
+
+            }
+
+            // TODO - implement this method
+        }
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,3 +1,5 @@
+import sun.net.www.MessageHeader;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +12,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +114,25 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if(someJobs.size() == 0) {
+            System.out.print("No Results");
+        }
+        for (HashMap<String, String> item : someJobs){
 
-        System.out.println("printJobs is not implemented yet");
+                System.out.println("\n*****");
+
+            for (Map.Entry<String, String> jobs : item.entrySet()) {
+                System.out.println(String.format("%s: %s", jobs.getKey(), jobs.getValue()));
+            }
+            System.out.println("*****");
+        }
+
     }
 }
